1b677b51b2a4577977686ca140571b21d9ff5c40
Xitonight <xitonight@gmail.com>
2025-04-13 21:58:09 +0200

    fix(tmux autohide): check for nvim only at the end
    prevent issues with different pane layouts

--- dots/.config/nvim/tmux.sh
+++ dots/.config/nvim/tmux.sh
@@ -4,13 +4,16 @@
 session_name=$(tmux display-message -p '#S')
 window_index=$(tmux display-message -p '#I')
 
-# Iterate over all panes in the current tmux window
-tmux list-panes -t "${session_name}:${window_index}" -F '#{pane_pid} #{pane_current_command}' | while read -r pane_pid pane_cmd; do
-  if [[ "$pane_cmd" == *"nvim"* ]] || pgrep -fla "^$pane_pid.*nvim" >/dev/null; then
-    tmux set status off
-    exit 0
-  else
-    tmux set status on
-    exit 0
+nvim_found=false
+
+while read -r _ pane_cmd; do
+  if [[ "$pane_cmd" == *"nvim"* ]]; then
+    nvim_found=true
   fi
-done
+done < <(tmux list-panes -t "${session_name}:${window_index}" -F '#{pane_pid} #{pane_current_command}')
+
+if $nvim_found; then
+  tmux set -w status off
+else
+  tmux set -w status on
+fi
